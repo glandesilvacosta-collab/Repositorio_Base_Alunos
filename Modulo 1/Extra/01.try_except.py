ec022928e20365aeaab839a5a66c22f4b4a26cbf
Emanuel Mello <glandesilvacosta@gmail.com>
2025-09-24 16:20:06 -0300

    Delete Modulo 1/Extra/01.try_except.py

--- Modulo 1/Extra/01.try_except.py
+++
@@ -1,9 +0,0 @@
-# Utilizamos o try/except para apresentar uma excessão
-# de uma maneira ,
-
-
-try:
-    resultado = 10/10
-except:
-    print("Ocorreu um erro na operação. Não é possível a divisão com denominador igual a zero" \
-    "")
