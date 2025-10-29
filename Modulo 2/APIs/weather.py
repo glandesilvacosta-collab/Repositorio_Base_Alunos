25ac60efe0b4cb1a4442e0be0d484b35839a5904
Emanuel Mello <glandesilvacosta@gmail.com>
2025-10-29 16:10:53 -0300

    Add files via upload

---
+++ Modulo 2/APIs/weather.py
@@ -0,0 +1,32 @@
+# Criar um código que consuma uma api de clima e informe
+# a temperatura e a descrição do clima em um lugar especifico
+
+# Etapa
+# 1. Definir chave da API e o link da requisição
+
+import requests
+api_key ='2a1ac38a32354cb7b19133643251408' # Chave da API
+cidade = input('Digite o nome da cidade: ').strip() # Input para receber a informação do usuário e a função  
+# .strip() tira os espaços
+url=f'https://api.weatherapi.com/v1/current.json' # url da api
+
+# 2. Paramêtros da requisição
+parametros = {
+    'key':api_key,
+    'q':cidade,
+    'lang':'pt' # Define a lingua da resposta como português
+}
+
+# 3. Fazer a requisição
+resposta = requests.get(url, params=parametros) # Utilizamos o método get e informamos os parâmetros dessa requisição
+
+# 4. Verificar se a requisição foi bem sucedida
+if resposta.status_code == 200:
+    dados = resposta.json() # Convertendo a resposta JSON em um dicionário Python 
+    temperatura = dados['current']['temp_c']
+    descricao = dados['current']['condition']['text']
+    print(f'Temperatura na cidade {cidade} é {temperatura} °C.')
+    print(f'Descrição: {descricao}')
+else:
+    print(f'Erro na requisição: {resposta.status_code}')
+    print(resposta.content)
